5e75fb43fa0ba658043b7616144512c8509a9804
Trần Anh Quân <heart.smile.79@gmail.com>
2015-01-29 16:58:47 +0700

    new version by NTHQ group

--- qa-lang-emails.php
+++ qa-lang-emails.php
@@ -25,55 +25,69 @@
 */
 
 	return array(
-		'a_commented_body' => "Câu trả lời của bạn trên ^site_title có 1 bình luận mới bởi ^c_handle:\n\n^open^c_content^close\n\nCâu trả lời của bạn là:\n\n^open^c_context^close\n\nBạn có thể hồi âm bằng cách thêm bình luận của bạn tại đây:\n\n^url\n\nCảm ơn bạn,\n\n^site_title",
-		'a_commented_subject' => 'Có 1 bình luận mới về câu trả lời của bạn trên ^site_title',
+		'a_commented_body' => "Câu trả lời của bạn ^site_title đã được bình luận bởi ^c_handle:\n\n^open^c_content^close\n\nCâu trả lời của bạn là:\n\n^open^c_context^close\n\nBạn có thể trả lời bằng cách thêm bình luận tại đây:\n\n^url\n\nCảm ơn bạn,\n\n^site_title",
+		'a_commented_subject' => 'Đã có 1 bình luận mới về câu hỏi của bạn trên ^site_title',
 
-		'a_followed_body' => "Câu trả lời của bạn trên ^site_title có 1 câu hỏi liên quan bởi ^q_handle:\n\n^open^q_title^close\n\nCâu trả lời của bạn là:\n\n^open^a_content^close\n\nBấm vào đường link dưới đây để trả lời câu hỏi mới này:\n\n^url\n\nCảm ơn bạn,\n\n^site_title",
+		'a_followed_body' => "Câu trả lời của bạn trên ^site_title có một câu hỏi liên quan mới trên ^q_handle:\n\n^open^q_title^close\n\nCâu trả lời của bạn là:\n\n^open^a_content^close\n\nBấm vào dưới đây để trả lời các câu hỏi mới này:\n\n^url\n\nCảm ơn bạn,\n\n^site_title",
 		'a_followed_subject' => 'Có 1 câu hỏi liên quan đến câu trả lời của bạn trên ^site_title',
 
-		'a_selected_body' => "Xin chúc mừng! Câu trả lời của bạn trên ^site_title đã được bầu chọn là hay nhất bởi ^s_handle:\n\n^open^a_content^close\n\nCâu hỏi là:\n\n^open^q_title^close\n\nBấm vào đường link dưới đây để xem câu trả lời của bạn:\n\n^url\n\nCảm ơn bạn,\n\n^site_title",
-		'a_selected_subject' => 'Câu trả lời của bạn đã được bầu chọn là hay nhất trên ^site_title !',
+		'a_selected_body' => "Xin chúc mừng! Câu trả lời của bạn trên ^site_title đã được bình chọn là tốt nhất bởi ^s_handle:\n\n^open^a_content^close\n\nCâu hỏi là:\n\n^open^q_title^close\n\nBấm vào dưới đây để xem câu trả lời của bạn:\n\n^url\n\nCảm ơn bạn,\n\n^site_title",
+		'a_selected_subject' => 'Câu trả lời trên ^site_title của bạn đã được bình chọn là hay nhất!',
 
-		'c_commented_body' => "Một bình luận mới bởi ^c_handle đã được thêm vào sau bình luận của bạn trên ^site_title:\n\n^open^c_content^close\n\nCuộc thảo luận đang theo dõi:\n\n^open^c_context^close\n\nBạn có thể hồi âm bằng cách thêm 1 bình luận khác tại đây:\n\n^url\n\nCảm ơn bạn,\n\n^site_title",
+		'c_commented_body' => "Một bình luận mới bởi ^c_handle đã được thêm vào sau khi bình luận của bạn trên ^site_title:\n\n^open^c_content^close\n\nCác cuộc thảo luận đang được theo dõi:\n\n^open^c_context^close\n\nBạn có thể trả lời bằng cách thêm một bình luận khác tại đây:\n\n^url\n\nCảm ơn bạn,\n\n^site_title",
 		'c_commented_subject' => 'Một bình luận vừa được thêm vào sau bình luận của bạn trên ^site_title',
 
-		'confirm_body' => "Xin vui lòng bấm vào đường link dưới đây để xác nhận địa chỉ Email của bạn tại ^site_title.\n\n^url\n\nCảm ơn bạn,\n^site_title",
-		'confirm_subject' => 'Xác nhận địa chỉ Email - ^site_title',
+		'confirm_body' => "Xin vui lòng bấm vào đường dẫn dưới đây để xác nhận địa chỉ thư điện tử của bạn tại ^site_title.\n\n^url\n\nCảm ơn,\n^site_title",
+		'confirm_subject' => '^site_title - Xác nhận địa chỉ thư điện tử.',
 
-		'feedback_body' => "Ý kiến phản hồi:\n^message\n\nTên:\n^name\n\nEmail:\n^email\n\nTrang trước đó:\n^previous\n\nThành viên:\n^url\n\nĐịa chỉ IP:\n^ip\n\nTrình duyệt:\n^browser",
-		'feedback_subject' => 'Ý kiến phản hồi từ ^',
+		'feedback_body' => "Bình luận:\n^message\n\nName:\n^name\n\nEmail:\n^email\n\nPrevious page:\n^previous\n\nUser:\n^url\n\nIP address:\n^ip\n\nBrowser:\n^browser",
+		'feedback_subject' => '^ Ý kiến phản hồi từ.',
 
-		'flagged_body' => "Một bài viết của ^p_handle đã nhận được ^flags:\n\n^open^p_context^close\n\nBấm vào link dưới đây để xem bài viết này:\n\n^url\n\nCảm ơn bạn,\n\n^site_title",
+		'flagged_body' => "Một bài đăng của ^p_handle đã nhận được ^flags:\n\n^open^p_context^close\n\nBấm vào link dưới đây để xem bài đăng này:\n\n^url\n\n\nBấm vào dưới đây để xem tất cả bài viết được đánh dấu:\n\n^a_url\n\n\nCảm ơn bạn,\n\n^site_title",
 		'flagged_subject' => 'Có 1 báo cáo vi phạm từ ^site_title',
 
-		'moderate_body' => "Một bài viết của ^p_handle yêu cầu sự chấp thuận của bạn:\n\n^open^p_context^close\n\nBấm vào link dưới đây để chấp thuận hoặc từ chối bài viết này:\n\n^url\n\nCảm ơn bạn,\n\n^site_title",
+		'moderate_body' => "Một bài đăng của ^p_handle yêu cầu bạn chấp nhận:\n\n^open^p_context^close\n\nBấm vào link dưới đây để chấp nhận hoặc từ chối các bài đăng:\n\n^url\n\n\nBấm vào dưới đây để xem tất cả bài đăng đang đợi:\n\n^a_url\n\n\nCảm ơn bạn,\n\n^site_title",
 		'moderate_subject' => 'Duyệt bài viết từ ^site_title',
 
-		'new_password_body' => "Mật khẩu mới của bạn tại ^site_title:\n\nMật khẩu: ^password\n\nChúng tôi khuyến cáo bạn nên đổi mật khẩu này ngay lập tức sau khi đăng nhập.\n\nCảm ơn bạn,\n^site_title\n^url",
-		'new_password_subject' => 'Mật khẩu mới của bạn tại ^site_title',
+		'new_password_body' => "Mật khẩu mới của bạn tại ^site_title.\n\nPassword: ^password\n\nBạn nên thay đổi mật khẩu này ngay sau khi đăng nhập.\n\nCảm ơn bạn,\n^site_title\n^url",
+		'new_password_subject' => '^site_title - Mật khẩu mới của bạn.',
 
-		'private_message_body' => "Bạn đã nhận được 1 tin nhắn cá nhân từ ^f_handle trên ^site_title:\n\n^open^message^close\n\n^moreCảm ơn bạn,\n\n^site_title\n\n\nĐể khóa chức năng nhắn tin cá nhân, bạn hãy vào đây để cài đặt:\n^a_url",
-		'private_message_info' => "Xem thêm thông tin về ^f_handle tại đây:\n\n^url\n\n",
-		'private_message_reply' => "Bấm vào link dưới đây để hồi âm tới ^f_handle:\n\n^url\n\n",
-		'private_message_subject' => 'Tin nhắn từ ^f_handle trên ^site_title',
+		'private_message_body' => "Bạn đã nhận một tin nhắn cá nhân từ ^f_handle trên ^site_title:\n\n^open^message^close\n\n^moreCảm ơn bạn,\n\n^site_title\n\n\nĐể chặn tin nhắn , hãy truy cập vào đây để cài đặt:\n^a_url",
+		'private_message_info' => "Xem thông tin thêm về ^f_handle tại đây:\n\n^url\n\n",
+		'private_message_reply' => "Bấm vào đường đãn dưới đây để trả lời tin nhắn của ^f_handle:\n\n^url\n\n",
+		'private_message_subject' => 'Có tin nhắn từ ^f_handle trên ^site_title',
 
-		'q_answered_body' => "Câu hỏi của bạn trên ^site_title đã được trả lời bởi ^a_handle:\n\n^open^a_content^close\n\nCâu hỏi của bạn là:\n\n^open^q_title^close\n\nNếu bạn thích câu trả lời này, bạn có thể bầu chọn nó là câu trả lời hay nhất tại đây:\n\n^url\n\nCảm ơn bạn,\n\n^site_title",
-		'q_answered_subject' => 'Câu hỏi của bạn đã được trả lời trên ^site_title',
+		'q_answered_body' => "Câu hỏi của bạn trên ^site_title đã được trả lời bởi ^a_handle:\n\n^open^a_content^close\n\nCâu hỏi của bạn là:\n\n^open^q_title^close\n\nNếu bạn thích câu trả lời này , bạn có thể bình chọn nó là câu trả lời hay nhất tại đây:\n\n^url\n\nCảm ơn bạn,\n\n^site_title",
+		'q_answered_subject' => 'Câu hỏi trên ^site_title của bạn đã được trả lời',
 
-		'q_commented_body' => "Câu hỏi của bạn trên ^site_title có 1 bình luận mới của ^c_handle:\n\n^open^c_content^close\n\nCâu hỏi của bạn là:\n\n^open^c_context^close\n\nBạn có thể hồi âm bằng cách thêm bình luận của bạn tại đây:\n\n^url\n\nCảm ơn bạn,\n\n^site_title",
-		'q_commented_subject' => 'Có 1 bình luận mới về câu hỏi của bạn trên ^site_title',
+		'q_commented_body' => "Câu hỏi của bạn trên ^site_title có bình luận mới bởi ^c_handle:\n\n^open^c_content^close\n\nCâu hỏi của bạn là:\n\n^open^c_context^close\n\nBạn có thể trả lời bằng cách thêm nhận xét của mình tại đây:\n\n^url\n\nCảm ơn bạn,\n\n^site_title",
+		'q_commented_subject' => 'Câu hỏi trên ^site_title của bạn đã có bình luận mới',
 
-		'q_posted_body' => "Một câu hỏi mới của ^q_handle:\n\n^open^q_title\n\n^q_content^close\n\nBấm vào đường link dưới đây để xem câu hỏi:\n\n^url\n\nCảm ơn bạn,\n\n^site_title",
-		'q_posted_subject' => 'Có 1 câu hỏi mới từ ^site_title',
+		'q_posted_body' => "Một câu hỏi mới đã được yêu cầu bởi ^q_handle:\n\n^open^q_title\n\n^q_content^close\n\nBấm vào đường dẫn dưới đây để xem các câu hỏi:\n\n^url\n\nCảm ơn bạn,\n\n^site_title",
+		'q_posted_subject' => 'Có một câu hỏi mới từ ^site_title',
+		
+		'remoderate_body' => "Một bài viết bởi ^p_handle cần bạn xem qua:\n\n^open^p_context^close\n\nBấm vào đường dẫn dưới đây để phê duyệt hoặc ẩn bài viết:\n\n^url\n\n\nBấm vào đường link dưới đây để xem câu hỏi:\n\n^a_url\n\n\nCảm ơn bạn,\n\n^site_title",
+		'remoderate_subject' => 'Duyệt bài viết từ ^site_title',
 
-		'reset_body' => "Xin vui lòng bấm vào link dưới đây để khôi phục lại mật khẩu của bạn tại ^site_title.\n\n^url\n\nNgoài ra, bạn có thể điền mã dưới đây vào trong ô trống.\n\nMã: ^code\n\nNếu bạn không yêu cầu khôi phục lại mật khẩu, xin vui lòng bỏ qua tin nhắn này.\n\nCảm ơn bạn,\n^site_title",
-		'reset_subject' => 'Khôi phục lại mật khẩu đã quên tại ^site_title',
+		'reset_body' => "Vui lòng bấm vào dưới đây để khôi phục lại mật khẩu của bạn tại ^site_title.\n\n^url\n\nNgoài ra, nhập mã dưới đây vào ô trống.\n\nCode: ^code\n\nNếu bạn không yêu cầu khôi phục lại mật khẩu, xin vui lòng bỏ qua thư này.\n\nCảm ơn bạn,\n^site_title",
+		'reset_subject' => '^site_title - Khôi phục lại mật khẩu bị mất.',
 
 		'to_handle_prefix' => "^,\n\n",
+		
+		'u_registered_body' => "Một tài khoản mới ^u_handle đã đăng ký.\n\nBấm vào đường dẫn dưới đây để xem hồ sơ người dùng:\n\n^url\n\nCảm ơn bạn,\n\n^site_title",
+		'u_to_approve_body' => "Một tài khoản mới ^u_handle đã đăng ký.\n\nBấm vào đường dẫn dưới đây để chấp nhận người sử dụng:\n\n^url\n\nBấm vào đường dẫn dưới đây để xem tất cả tài khoản mới đang chờ phê duyệt:\n\n^a_url\n\nCảm ơn bạn,\n\n^site_title",
+		'u_registered_subject' => 'Có tài khoản mới đăng ký trên ^site_title',
+		
+		'u_approved_body' => "Bạn có thể xem hồ sơ tài khoản mới ở đây:\n\n^url\n\nCảm ơn bạn,\n\n^site_title",
+		'u_approved_subject' => 'Tài khoản mới trên ^site_title đã được chấp nhận',
+		
+		'wall_post_subject' => 'Có bài đăng trên tường của bạn tại ^site_title',
+		'wall_post_body' => "^f_handle đã đăng trên tường của bạn tại ^site_title:\n\n^open^post^close\n\nBạn có thể trả lời cho bài viết ở đây:\n\n^url\n\nCảm ơn bạn,\n\n^site_title",
 
-		'welcome_body' => "Cảm ơn bạn đã đăng ký tài khoản tại ^site_title.\n\n^custom^confirmDưới đây là thông tin đăng nhập của bạn:\n\nEmail: ^email\nMật khẩu: ^password\n\nXin vui lòng giữ thông tin này an toàn để có thể tham khảo trong tương lai.\n\nCảm ơn bạn,\n\n^site_title\n^url",
-		'welcome_confirm' => "Xin vui lòng bấm vào đường link dưới đây để xác nhận địa chỉ Email của bạn:\n\n^url\n\n",
+		'welcome_body' => "Cảm ơn bạn đã đăng ký tài khoản tại ^site_title.\n\n^custom^confirmDưới đây là thông tin đăng nhập chi tiết của bạn:\n\nUsername: ^handle\nEmail: ^email\n\nHãy giữ những thông tin này an toàn để có thể dùng đến trong tương lai.\n\nCảm ơn bạn,\n\n^site_title\n^url",
+		'welcome_confirm' => "Vui lòng bấm vào đường dẫn dưới đây để xác nhận địa chỉ thư điện tử của bạn.\n\n^url\n\n",
 		'welcome_subject' => 'Chào mừng bạn đến với ^site_title!',
+
 	);
 	
 
